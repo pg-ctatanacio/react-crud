9a73f01f722e96aa5b9e87f5fb05c1b7059bacc9
Cris T. Atanacio IT-HO <ctatanacio@puregold.local>
2024-02-22 17:50:36 +0800

    Initial commit

---
+++ src/utils/firebase.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+let firebaseConfig = {
+	apiKey: process.env.REACT_APP_FB_API_KEY,
+    authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FB_DB_URL,
+    projectId: process.env.REACT_APP_FB_PROJ_ID,
+    storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FB_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FB_APP_ID,
+    measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default getDatabase(app);
