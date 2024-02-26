138dc84e6ef16b5e71902373f08fda95ed8d5cf9
Cris T. Atanacio IT-HO <ctatanacio@puregold.local>
2024-02-26 21:40:00 +0800

    Update item list design

--- src/pages/Items/Items.tsx
+++ src/pages/Items/Items.tsx
@@ -30,10 +30,6 @@ const Items = () => {
 		return onValue(query, (snapshot) => {
 			const data = snapshot.val();
 
-            // console.log(data);
-            // data.map((v:  any) => {
-            //     console.log(v);
-            // })
             let itemRows: any = [];
             Object.keys(data).forEach((k) => {
                 let rowObj = data[k];
@@ -48,30 +44,21 @@ const Items = () => {
                     itemRow.price = v.price;
                     itemRow.cost = v.cost;
                     itemRow.stocks = v.stocks;
-
-                    // itemRows.push(itemRow);
-
                     return itemRow;
                 })
 
                 itemRows = [...itemRows, ...row];
             })
 
-            console.log(itemRows);
-
             setRows(itemRows);
-
-			// setCategories(Object.values(data));
 		});
 	}, []);
 
     const handleEditClick = (id: any) => {
         handleOpenEditModal();
-        console.log(id);
     }
 
     const renderButton = (params: GridRenderCellParams<any, any, any, GridTreeNodeWithRender>) => {
-        // console.log(params.id);
         return (
             <Button onClick={() => handleEditClick(params.id)}>Test</Button>
         )
@@ -79,7 +66,7 @@ const Items = () => {
 
     const itemColumns: GridColDef[] = [
         { field: "category", headerName: "Category", width: 180 },
-        { field: "name", headerName: "Name", width: 200 },
+        { field: "name", headerName: "Name", flex: 1 },
         { field: "size", headerName: "Size", width: 100 },
         {
             field: "price",
@@ -103,6 +90,7 @@ const Items = () => {
             field: "id",
             headerName: "",
             width: 130,
+            align: 'right',
             renderCell: renderButton
         }
     ];
@@ -146,59 +134,6 @@ const Items = () => {
             </Modal>
         </Container>
     );
-};
-
-const columns: GridColDef[] = [
-	{ field: "id", headerName: "ID", width: 70 },
-	{ field: "firstName", headerName: "First name", width: 130 },
-	{ field: "lastName", headerName: "Last name", width: 130 },
-	{
-		field: "age",
-		headerName: "Age",
-		type: "number",
-		width: 90,
-	},
-	{
-		field: "fullName",
-		headerName: "Full name",
-		description: "This column has a value getter and is not sortable.",
-		sortable: false,
-		width: 160,
-		valueGetter: (params: GridValueGetterParams) =>
-			`${params.row.firstName || ""} ${params.row.lastName || ""}`,
-	},
-];
-
-
-
-const rows = [
-	{ id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-	{ id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-	{ id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-	{ id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-	{ id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-	{ id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-	{ id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-	{ id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-	{ id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
-
-const DataTable = () => {
-	return (
-		<div style={{ height: 400, width: "100%" }}>
-			<DataGrid
-				rows={rows}
-				columns={columns}
-				initialState={{
-					pagination: {
-						paginationModel: { page: 0, pageSize: 5 },
-					},
-				}}
-				pageSizeOptions={[5, 10]}
-				checkboxSelection
-			/>
-		</div>
-	);
 };
 
 export default Items;
